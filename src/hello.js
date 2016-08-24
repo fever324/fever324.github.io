487f5a1d2df9372e17c4e5d5fc96b70ba73da6b0
Hongfei Li <fever324@gmail.com>
2016-08-24 16:59:42 -0400

    Initial commit

---
+++ src/hello.js
@@ -0,0 +1,19 @@
+class Name extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Building... </h1>
+        <h2>建设中...</h2>
+        <br/>
+        <br/>
+        <br/>
+        <h3>Hongfei Li, MPS @ Cornell Info Science</h3>
+        <h3>hl963@cornell.edu</h3>
+      </div>
+    )
+  }
+}
+ReactDOM.render(
+  <Name/>,
+  document.getElementById('container')
+);
